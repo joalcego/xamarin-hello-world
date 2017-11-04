55db31cfe3431cc58cbf1acea4c7938b62c83922
Jose Cerdas <jcerdasg@ucenfotec.ac.cr>
2017-11-03 18:51:37 -0600

    First commit with initial files

---
+++ xamarin_hello_world/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace xamarin_hello_world
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ xamarin_hello_world/ViewModels/AboutViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace xamarin_hello_world
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
